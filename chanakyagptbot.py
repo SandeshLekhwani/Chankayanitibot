8b1888edcbcedd2f51c97eb26b68c143d8023245
Sandesh Lekhwani <sandeshlekhwani@gmail.com>
2023-08-27 12:29:42 +0530

    Update and rename widurgptbot.py to chanakyagptbot.py

--- chanakyagptbot.py
+++ chanakyagptbot.py
@@ -4,27 +4,27 @@ from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
 
-st.set_page_config(page_title="Chat with Widur", page_icon="🧙‍♂️", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat with Chanakya", page_icon="👨🏻‍🏫", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Vidur Niti")
+st.title("Chanakya Niti")
 #st.session_state.clear()
 
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Tell me what are you thinking about 🧙‍♂️"}
+        {"role": "assistant", "content": "Tell me what you are thinking about 👨🏻‍🏫"}
     ]
 
 st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing Vidur Niti – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing Chanakya Niti 📜 – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on Vidur Niti and can impersonate Vidur. Formulate your answers based on Vidur Niti – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on Chankaya Niti and can impersonate Chankaya. Formulate your answers based on Chankaya Niti – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
 index = load_data()
-chat_engine = index.as_chat_engine(chat_mode="context", verbose=True, system_prompt="You are an expert on Vidur Niti and can impersonate Vidur. Formulate your answers based on Vidur Niti – do not hallucinate features.")
+chat_engine = index.as_chat_engine(chat_mode="context", verbose=True, system_prompt="You are an expert on Chanakya Niti and can impersonate Chanakya. Formulate your answers based on Chanakya Niti – do not hallucinate features.")
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
